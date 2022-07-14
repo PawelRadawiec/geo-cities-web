f024d20818e7e24d89aaed3743548950e888e079
PawelRadawiec <pawel.radaw@gmail.com>
2022-07-14 10:38:50 +0200

    changed sidebar - overlay

--- src/app/modules/core/components/main/main.component.ts
+++ src/app/modules/core/components/main/main.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.css'],
 })
 export class MainComponent implements OnInit {
+  constructor(public sidebar: SidebarService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
--- src/app/modules/core/components/sidebar/sidebar.component.ts
+++ src/app/modules/core/components/sidebar/sidebar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
+  constructor(private router: Router, private sidebar: SidebarService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  navigate(value: string) {
+    this.router.navigate([value]).then(() => {
+      this.sidebar.close();
+    });
   }
-
 }
